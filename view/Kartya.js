df8fbb3b4fa52348953e25a5aaf44f079eb09b93
AlexandraLukacs <lszandika15@gmail.com>
2024-11-10 14:26:03 +0100

    javitas probalkozas

--- view/Kartya.js
+++ view/Kartya.js
@@ -7,37 +7,24 @@ export default class Kartya{
     constructor(index, szuloElem){
         this.#index=index;
         this.#szuloElem=szuloElem;
-        this.elem= this.kartyaKiir();
+        this.#elem=this.kartyaKiir();
         this.esemenyKezelo();
     }
 
-    kartyaKiir(){
-        // Kártya elem létrehozása
-        const kartyaElem = document.createElement('div');
-        kartyaElem.classList.add('kartya');
-
-        // Kártya body létrehozása
-        const kartyaBody = document.createElement('div');
-        kartyaBody.classList.add('kartya-body');
-
-        // Kép elem létrehozása
-        const kartyaImg = document.createElement('img');
-        kartyaImg.classList.add('kartya-img');
-        kartyaImg.setAttribute('src', '');
-        kartyaImg.setAttribute('alt', 'ordogszellem');
-
-        // A kártya struktúra felépítése
-        kartyaBody.appendChild(kartyaImg);
-        kartyaElem.appendChild(kartyaBody);
-
-        // A kártya hozzáadása a szülő elemhez
-        this.#szuloElem.appendChild(kartyaElem);
-
-        return kartyaElem;
+    termekKiir(){
+        this.#szuloElem.append(`
+            <div class="divek">
+                <div class="card">
+                    <p class="kivalaszt">
+                    
+                    </p>
+                </div>
+            </div>
+            `)
     }
 
     esemenyKezelo(){
-        this.elem.on("click", ()=>{
+        this.#elem.on("click", ()=>{
             const e=new CustomEvent("kivalaszt", {detail:this.#index})
             window.dispatchEvent(e)
         })
